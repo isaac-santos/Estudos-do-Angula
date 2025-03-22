3b2c2651565591fe2414b62866f6fab7644f5892
isaac-santos <isaacconceicao26@gmail.com>
2025-03-22 20:09:56 -0300

    fiz a primeira para do projeto CRUD

--- angular-basico/src/app/meu-componente11/meu-componente11.component.ts
+++ angular-basico/src/app/meu-componente11/meu-componente11.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-meu-componente11',
@@ -9,5 +9,12 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './meu-componente11.component.css'
 })
 export class MeuComponente11Component {
+  
+  // Objeto de formulário
+  formulario = new FormGroup({
+    nome   : new FormControl('', [Validators.required,   Validators.minLength(3)]),
+    idade  : new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+    cidade : new FormControl('', [Validators.required,   Validators.minLength(3)])
+  });
 
 }
