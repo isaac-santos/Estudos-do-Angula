6b165a4b60fc5114ca194b5155f033835279c633
isaac-santos <isaacconceicao26@gmail.com>
2025-05-10 19:52:20 -0300

    adicionei os produtos ao projeto

--- angular-basico/src/app/meu-componente13/meu-componente13.component.ts
+++ angular-basico/src/app/meu-componente13/meu-componente13.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Produto } from '../modelo/Produto';
+import { ProdutoService } from '../servico/produto.service';
 
 @Component({
   selector: 'app-meu-componente13',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './meu-componente13.component.html',
   styleUrl: './meu-componente13.component.css'
 })
 export class MeuComponente13Component {
+
+  // Vetor
+  vetor:Produto[] = [];
+
+  // Construtor
+  constructor(private servico:ProdutoService){}
+
+  // Inicialização do componente
+  ngOnInit(){
+    this.selecionar();
+  }
+
+  // Método para selecionar todos os produtos
+  selecionar(){
+    this.servico.selecionar().subscribe(retorno => {this.vetor = retorno});
+  }
 
 }
