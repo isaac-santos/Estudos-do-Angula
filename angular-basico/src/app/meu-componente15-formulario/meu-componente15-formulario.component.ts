9b812cb16142e3a9ba0bff50388d2cf26cf18e27
isaac-santos <isaacconceicao26@gmail.com>
2025-06-23 19:29:55 -0300

    adicionei a campo e botão para cadastro

--- angular-basico/src/app/meu-componente15-formulario/meu-componente15-formulario.component.ts
+++ angular-basico/src/app/meu-componente15-formulario/meu-componente15-formulario.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-meu-componente15-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './meu-componente15-formulario.component.html',
   styleUrl: './meu-componente15-formulario.component.css'
 })
 export class MeuComponente15FormularioComponent {
 
+  // Variável para cadastrar nome do prato
+  menu:string = '';
+
+  // Output
+  @Output() funcao = new EventEmitter<string>(); // dentro do maior e menor vamos passar o tipo de parâmetro que função precisa
+
+  // Função de cadastro do componente de formulário
+  cadastrarPrato(){
+    // aqui utilizamos a função criada no componente html
+    this.funcao.emit(this.menu);
+
+    
+  }
 }
