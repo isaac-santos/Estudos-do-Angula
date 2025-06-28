4cbcecaa4864d103c78ea3bf3976e270b55080bb
isaac-santos <isaacconceicao26@gmail.com>
2025-06-28 20:55:46 -0300

    ativei a rota no componente e criei a segurança

--- angular-basico/src/app/meu-componente16/meu-componente16.component.ts
+++ angular-basico/src/app/meu-componente16/meu-componente16.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-meu-componente16',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './meu-componente16.component.html',
   styleUrl: './meu-componente16.component.css'
 })
