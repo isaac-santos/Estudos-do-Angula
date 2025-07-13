04aa72de8e177f02c7cdc4d80396d5c193b46286
isaac-santos <isaacconceicao26@gmail.com>
2025-07-13 00:14:11 -0300

    aula de redirecionamento

--- angular-basico/src/app/autenticacao/rota.guard.ts
+++ angular-basico/src/app/autenticacao/rota.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
 
 export const rotaGuard: CanActivateFn = (route, state) => {
-  return true;
+  
+  // Constante
+  const rota = inject(Router);
+
+  // Redirecionamento
+  //rota.navigateByUrl('/pagina1');
+  if(localStorage.getItem('nome') === 'Isaac'){
+    return true;
+  }else{
+    return rota.parseUrl('/pagina1');
+  }
+  
 };
