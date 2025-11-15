9873298c628b8c17f80aa86a16d7d0b3ef610f6e
isaac-santos <isaacconceicao26@gmail.com>
2025-11-15 01:04:35 -0300

    Menu do cabeçalho(header Menu)
    Adicionei o menu do angular material e alterei os nomes dos módulos (i aplicated the menu at the header and alter the model's name).

--- cProject/src/app/app.component.ts
+++ cProject/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { ProfileDialogComponent } from './profile-dialog/profile-dialog.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatMenuModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,6 +18,10 @@ export class AppComponent {
   readonly dialog = inject(MatDialog);
 
   openDialog():void{
-    this.dialog.open(ProfileDialogComponent);
+    let dialogRef = this.dialog.open(ProfileDialogComponent, {
+      height: '400px',
+      width: '600px',
+
+    });
   }
 }
--- cProject/src/app/profile-dialog/profile-dialog.component.ts
+++ cProject/src/app/profile-dialog/profile-dialog.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-profile-dialog',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, MatExpansionModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './profile-dialog.component.html',
   styleUrl: './profile-dialog.component.css'
 })
 export class ProfileDialogComponent {
-
+  readonly panelOpenState = signal(false);
 }
